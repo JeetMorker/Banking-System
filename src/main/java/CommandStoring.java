cf7e1b7437d94a222acbadd81aa59f45d152918e
JeetMorker <jeetmorker11@gmail.com>
2024-11-07 16:02:16 -0500

    created command storing class and added test cases for invalid commands

---
+++ src/main/java/CommandStoring.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CommandStoring {
+	private List<String> invalidCommands;
+
+	CommandStoring() {
+		invalidCommands = new ArrayList<>();
+	}
+
+	public void addInvalidCommand(String command) {
+		invalidCommands.add(command);
+	}
+
+	public List<String> getInvalidCommands() {
+		return invalidCommands;
+	}
+
+}
