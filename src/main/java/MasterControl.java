88ab0bf9b63601be9deb78e5298dd2bcde2df5aa
JeetMorker <jeetmorker11@gmail.com>
2024-11-07 21:19:22 -0500

    added master control class and test cases

---
+++ src/main/java/MasterControl.java
@@ -0,0 +1,25 @@
+import java.util.List;
+
+public class MasterControl {
+	private AllCommandValidator commandValidator;
+	private AllCommandProcessor commandProcessor;
+	private CommandStoring commandStorage;
+
+	public MasterControl(AllCommandValidator commandValidator, AllCommandProcessor commandProcessor,
+			CommandStoring commandStorage) {
+		this.commandValidator = commandValidator;
+		this.commandProcessor = commandProcessor;
+		this.commandStorage = commandStorage;
+	}
+
+	public List<String> start(List<String> input) {
+		for (String command : input) {
+			if (commandValidator.validate(command)) {
+				commandProcessor.process(command);
+			} else {
+				commandStorage.addInvalidCommand(command);
+			}
+		}
+		return commandStorage.getInvalidCommands();
+	}
+}
